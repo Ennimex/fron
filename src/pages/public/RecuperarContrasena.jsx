c49757b0489ad19026c1fe2d9ad426bc5a7bf60f
Ennimex <alexanderhernan325@gmail.com>
2026-03-25 15:57:14 -0600

    feat: agregar vista de recuperación de contraseña v2

--- src/pages/public/RecuperarContrasena.jsx
+++ src/pages/public/RecuperarContrasena.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo} from "react";
 import { IonIcon } from "@ionic/react";
 import { mailOutline, arrowBackOutline, checkmarkCircleOutline, keyOutline } from "ionicons/icons";
 import { Link } from "react-router-dom";
